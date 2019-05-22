4b01e55d1986b5d927562b9c9a53eacb200591bc
Amin Hamdane <you@example.com>
2019-05-22 09:56:34 +0000

    ?

--- Aufgabe1.py
+++ Aufgabe1.py
@@ -1,22 +1,24 @@
 def r_fac(n):
 	if n == 1:
 		return 1
+	if n == 0:
+		return 1
 	return n*r_fac(n-1)
 
 def i_fac(var):
+	if var == 0:
+		return 1
 	result = 1
-	for i in range(1,var):
+	for i in range(1,var+1):
 		result = result*i
-	return result*var
+	return result
 
 
-
-var = int(input())
+var = int(input("Geben Sie eine Zahl ein : "))
 signal = True
 while signal:
 	if var < 0:
-		print("Ihre Eingabe war keine natürliche Zahl! Versuchen Sie es nochmal!!!1")
-		var = int(input())
+		var = int(input("Ihre Eingabe war keine natürliche Zahl! Versuchen Sie es nochmal!!!1 : "))
 	else:
 		signal = False
 		print("Die Fakultät von", var , "ist", r_fac(var) )
