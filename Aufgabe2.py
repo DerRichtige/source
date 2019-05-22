ce53e6949836758912e01303e9a323ccc2d128a0
Amin Hamdane <you@example.com>
2019-05-22 13:00:14 +0000

    ?

---
+++ Aufgabe2.py
@@ -0,0 +1,29 @@
+def r_fib(n):
+	if n == 1:
+		return 1
+	if n == 0:
+		return 0
+	return r_fib(n-1)+r_fib(n-2)
+
+def i_fib(n):
+	a, b, count = 0, 1, 0
+	while count < n:
+		a, b = b, a+b
+		count += 1
+	return a
+
+var = int(input("Geben Sie eine Fibonacci Zahl ein : "))
+
+while True:
+	if var < 0:
+		var = int(input("Geben Sie eine Fibonacci Zahl ein : "))
+		continue
+	else:
+		print("Die ", var, "te Fibonacci Zahl ist : ", r_fib(var), "(Rekursiv)")
+		print("Die ", var, "te Fibonacci Zahl ist : ", i_fib(var), "(Iterativ)")
+		break
+
+
+print(r_fib(var))
+print(i_fib(var))
+	
